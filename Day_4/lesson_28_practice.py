282274dc1f9a8995c810b4c654856080d67572bf
Ella Watkins <ewatkins@caltech.edu>
2016-09-14 13:10:59 -0700

    Re-established .bashrc file., fixed PYTHONPATH

--- Day_4/lesson_28_practice.py
+++ Day_4/lesson_28_practice.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import bootcamp_utils as bc_ut
 import seaborn as sns
+
 sns.set()
 
 def draw_bs_reps(data, func, size=1):
@@ -42,3 +44,15 @@ def confinterval95(data, func, size=1):
             bs_replicates[i]=func(bs_sample)
     conf_int= np.percentile(bs_replicates, [2.5, 97.5])
     return conf_int
+
+##########Practice 2: Plot EXDFs of bootstrap samples#####
+
+bd_1975= np.loadtxt('../data/beak_depth_scandens_1975.csv')
+x_1975, y_1975=bc_ut.ecdf(bd_1975)
+plt.close()
+
+plt.plot(x_1975, y_1975, marker='.', linestyle='none')
+plt.xlabel('beak depth (mm)')
+plt.ylabel('ECDF')
+plt.legend(('1975'), loc= 'lower right')
+plt.show()
